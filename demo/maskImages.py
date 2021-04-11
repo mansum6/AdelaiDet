6c5f813bc680c6a5cc93daee367e7f870f4c296b
mansum6 <31364532+mansum6@users.noreply.github.com>
2021-04-11 17:07:21 -0600

    Update maskImages.py

--- demo/maskImages.py
+++ demo/maskImages.py
@@ -98,10 +98,6 @@ if __name__ == "__main__":
               *******  
             
             # Inference with a keypoint detection model
-            cfg = get_cfg()   # get a fresh new config
-            cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"))
-            cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set threshold for this model
-            cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml")
             predictor = DefaultPredictor(cfg)
 
             outputs = predictor(im)
